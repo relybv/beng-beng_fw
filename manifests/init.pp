8f1ea91719877ceb78a8234f823807a0b3fe99ad
Marco Sjerps <msjerps@beeldengeluid.nl>
2016-12-06 15:49:48 +0100

    Init parameter consolideren en gelijk maken

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,26 +12,22 @@ class beng_fw
   $tcp_public_ports = $::beng_fw::params::tcp_public_ports,
   $tcp_ports_global = $::beng_fw::params::tcp_ports_global,
   $tcp_extra_rule1 = $::beng_fw::params::tcp_extra_rule1,
-  $tcp_rangea_src1 = $::beng_fw::params::tcp_rangea_src1,
-  $tcp_rangea_src2 = $::beng_fw::params::tcp_rangea_src2,
-  $tcp_rangea_src3 = $::beng_fw::params::tcp_rangea_src3,
-  $tcp_rangeb = $::beng_fw::params::tcp_rangeb,
-  $tcp_rangec = $::beng_fw::params::tcp_rangec,
- # parameter uit puppet v3 code
+  $tcp_rangeA_src1 = $::beng_fw::params::tcp_rangeA_src1,
+  $tcp_rangeA_src2 = $::beng_fw::params::tcp_rangeA_src2,
+  $tcp_rangeA_src3 = $::beng_fw::params::tcp_rangeA_src3,
+  $tcp_rangeB = $::beng_fw::params::tcp_rangeB,
+  $tcp_rangeC = $::beng_fw::params::tcp_rangeC,
+# parameter uit puppet v3 code
   $internal_netA    = '172.18.0.0/16',
   $internal_netB    = '172.19.0.0/16',
   $internal_netC    = '178.249.248.128/25',
-  $tcp_ports        = $beng_fw::tcp_ports_global, 
-  
-
-  # Port range 9300-9400, with variable source rules
+  $tcp_ports        = $::beng_fw::tcp_ports_global, 
+# Port range 9300-9400, with variable source rules
   $tcp_rangeA_ports = '9300-9400',
-  $tcp_rangeA_src1  = $beng_fw::tcp_9300_source1,
-  $tcp_rangeA_src2  = $beng_fw::tcp_9300_source2,
-  $tcp_rangeA_src3  = $beng_fw::tcp_9300_source3,
-
-  $tcp_rangeB = $beng_fw::tcp_rangeB,    
-  $tcp_rangeC = $beng_fw::tcp_rangeC,    
+  $tcp_rangeA_src1  = $::beng_fw::tcp_9300_source1,
+  $tcp_rangeA_src2  = $::beng_fw::tcp_9300_source2,
+  $tcp_rangeA_src3  = $::beng_fw::tcp_9300_source3,
+# UDP ports
   $udp_ports = [ '53','123','161'],
 ) inherits ::beng_fw::params {
 
