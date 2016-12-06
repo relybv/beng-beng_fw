2262ec8d10efc45ffe40356e28509bbc37fcfb79
Marco Sjerps <msjerps@beeldengeluid.nl>
2016-12-06 10:20:24 +0100

    update postv4 init copy from puppetv3

--- manifests/postv4.pp
+++ manifests/postv4.pp
@@ -8,4 +8,35 @@ class beng_fw::postv4 {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
+  firewall { '900 log all drop connections':
+    proto      => 'all',
+    jump       => 'LOG',
+    limit      => '5/min',
+    log_prefix => 'IPTables-Rejected: ',
+    provider   => 'iptables',
+  }->
+  firewall { '950 drop udp':
+    proto    => 'udp',
+    reject   => 'icmp-port-unreachable',
+    action   => 'reject',
+    provider => 'iptables',
+  }->
+  firewall { '951 drop tcp':
+    proto    => 'tcp',
+    reject   => 'tcp-reset',
+    action   => 'reject',
+    provider => 'iptables',
+  }->
+  firewall { '952 drop icmp':
+    proto    => 'icmp',
+    action   => 'drop',
+    provider => 'iptables',
+  }->
+  firewall { '999 drop everything else - this is the failsafe rule':
+    proto    => 'all',
+    action   => 'drop',
+    provider => 'iptables',
+    before   => undef,
+  }
+
 }
