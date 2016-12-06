860b896a356c05d978898232d46fec7e431dcf1e
Marco Sjerps <msjerps@beeldengeluid.nl>
2016-12-06 10:34:09 +0100

    vv dirict voor beng

--- files/bootme.sh
+++ files/bootme.sh
@@ -3,9 +3,9 @@
 # This script installs puppet 3.x or 4.x and deploy the manifest using puppet apply -e "include beng_fw"
 #
 # Usage:
-# Ubuntu / Debian: wget https://raw.githubusercontent.com/relybv/dirict-beng_fw/master/files/bootme.sh; bash bootme.sh
+# Ubuntu / Debian: wget https://raw.githubusercontent.com/relybv/beng-beng_fw/master/files/bootme.sh; bash bootme.sh
 #
-# Red Hat / CentOS: curl https://raw.githubusercontent.com/relybv/dirict-beng_fw/master/files/bootme.sh -o bootme.sh; bash bootme.sh
+# Red Hat / CentOS: curl https://raw.githubusercontent.com/relybv/beng-beng_fw/master/files/bootme.sh -o bootme.sh; bash bootme.sh
 # Options: add 3 as parameter to install 4.x release
 
 # default major version, comment to install puppet 3.x
