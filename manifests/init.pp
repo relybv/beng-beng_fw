f49963b5ee18de46955a291ba82066cc8d89b843
Marco Sjerps <msjerps@beeldengeluid.nl>
2016-12-06 10:30:16 +0100

    added pup[petv3 parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,6 +17,23 @@ class beng_fw
   $tcp_rangea_src3 = $::beng_fw::params::tcp_rangea_src3,
   $tcp_rangeb = $::beng_fw::params::tcp_rangeb,
   $tcp_rangec = $::beng_fw::params::tcp_rangec,
+ # parameter uit puppet v3 code
+  $internal_netA    = '172.18.0.0/16',
+  $internal_netB    = '172.19.0.0/16',
+  $internal_netC    = '178.249.248.128/25',
+  $tcp_ports        = $beng_fw::tcp_ports_global, 
+  $tcp_public_ports = $beng_fw::tcp_public_ports,
+  $tcp_extra_rule1  = $beng_fw::tcp_extra_rule1,
+
+  # Port range 9300-9400, with variable source rules
+  $tcp_rangeA_ports = '9300-9400',
+  $tcp_rangeA_src1  = $beng_fw::tcp_9300_source1,
+  $tcp_rangeA_src2  = $beng_fw::tcp_9300_source2,
+  $tcp_rangeA_src3  = $beng_fw::tcp_9300_source3,
+
+  $tcp_rangeB = $beng_fw::tcp_rangeB,    
+  $tcp_rangeC = $beng_fw::tcp_rangeC,    
+  $udp_ports = [ '53','123','161'],
 ) inherits ::beng_fw::params {
 
   # validate parameters here
