f4dfad721c61d371657725eb16cc0510e76ec43c
Marco Sjerps <msjerps@beeldengeluid.nl>
2016-12-06 15:52:56 +0100

    tcp_rangA duplicate verwijderd

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,9 +24,9 @@ class beng_fw
   $tcp_ports        = $::beng_fw::tcp_ports_global, 
 # Port range 9300-9400, with variable source rules
   $tcp_rangeA_ports = '9300-9400',
-  $tcp_rangeA_src1  = $::beng_fw::tcp_9300_source1,
-  $tcp_rangeA_src2  = $::beng_fw::tcp_9300_source2,
-  $tcp_rangeA_src3  = $::beng_fw::tcp_9300_source3,
+  #$tcp_rangeA_src1  = $::beng_fw::tcp_9300_source1,
+  #$tcp_rangeA_src2  = $::beng_fw::tcp_9300_source2,
+  #$tcp_rangeA_src3  = $::beng_fw::tcp_9300_source3,
 # UDP ports
   $udp_ports = [ '53','123','161'],
 ) inherits ::beng_fw::params {
