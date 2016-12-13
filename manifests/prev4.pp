720481f6abcb02c02ad10bfc9527d8e84af64777
Marco Sjerps <msjerps@beeldengeluid.nl>
2016-12-13 15:00:07 +0100

    added tcp_extra rule2

--- manifests/prev4.pp
+++ manifests/prev4.pp
@@ -73,6 +73,15 @@ class beng_fw::prev4 {
       action   => 'accept',
       provider => 'iptables',
     }
+     # Check if extra rule 2 is specified
+  if $::beng_fw::tcp_extra_rule2 != false {
+    firewall { '014 allow internal net TCP':
+      dport    => $::beng_fw::tcp_extra_rule2_dport,
+      proto    => 'tcp',
+      source   => $::beng_fw::tcp_extra_rule2_source,
+      action   => 'accept',
+      provider => 'iptables',
+    }
   }
 
   # RANGE A
